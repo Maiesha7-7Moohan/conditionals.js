12ff24a693964b311093af638651e931bf287752
Maiesha Moohan <maieshamoohan@gmail.com>
2026-05-25 09:31:18 +0200

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,12 @@
+let score = 75;
+let PassingGrade = 60;
+
+if (score === 100) {
+  //== Can lead to confusing results, such as "" == 0 or false == 0 evaluating to true. Using === ensures that "" === 0 is false because a string is not a number.
+  console.log("Passed - Perfect Score!");
+  // Prints the text "Passed - Perfect Score!".
+} else if (score >= PassingGrade) {
+  console.log("Passed - Great Job!");
+} else {
+  console.log("Failed - Better luck next time!");
+}
